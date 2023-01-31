dafbb2c4cea2e1f65880af3a419dec4b08a6a63a
NguyenKhue <nguyenkhue09032001@gmail.com>
2023-02-01 01:39:29 +0700

    Test location tracking

--- app/src/main/java/com/khue/speedmodule/MainActivity.kt
+++ app/src/main/java/com/khue/speedmodule/MainActivity.kt
@@ -1,11 +1,162 @@
 package com.khue.speedmodule
 
+import android.Manifest
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.lifecycleScope
+import com.khue.speedmodule.databinding.ActivityMainBinding
+import com.khue.speedmodule.speedtracking.BackgroundLocationTrackingService
+import com.khue.speedmodule.speedtracking.LocationTrackingService
 
 class MainActivity : AppCompatActivity() {
+
+    private val permissionId = 3
+    private lateinit var locationService: BackgroundLocationTrackingService
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        locationService = BackgroundLocationTrackingService.getInstance(this.applicationContext)
+        binding.button.setOnClickListener {
+            locationService.stopLocationService()
+        }
+        startLocationTracking()
+        observeLocation()
     }
+
+    private fun observeLocation() {
+        lifecycleScope.launchWhenStarted {
+            locationService.location.collect {
+                Log.i("MainActivity", "Location: $it")
+                binding.textView.text = "Latitude: ${it?.latitude}  \n Longitude: ${it?.longitude} \n speed: ${(it?.speed)?.let { speed -> speed * 3.6 }} km/h - ${(it?.speed)} m/s"
+            }
+        }
+    }
+
+    private fun requestAccessBGLocationForAndroidQorHigher() {
+        runOnApiAbove(Build.VERSION_CODES.Q) {
+            if (!checkLocationAccessBG() && checkPermissions()) {
+                requestAccessBGLocation()
+            }
+        }
+    }
+
+    private fun startLocationTracking() {
+        requestAccessBGLocationForAndroidQorHigher()
+        if (checkPermissions() && isLocationEnabled()) {
+            startLocationService()
+        } else if (!checkPermissions()) {
+            requestPermissions()
+        } else if (!isLocationEnabled()) {
+            Toast.makeText(this, "Please turn on location", Toast.LENGTH_LONG).show()
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun startLocationService() {
+        if (!application.isServiceRunning(LocationTrackingService::class.java)) {
+            locationService.startLocationService()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun checkLocationAccessBG(): Boolean {
+        val isGranted =
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        Toast.makeText(this, "access bg $isGranted", Toast.LENGTH_LONG).show()
+        return isGranted
+    }
+
+    private fun checkPermissions(): Boolean {
+
+        return ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissions() {
+        val permissions = mutableListOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        )
+        ActivityCompat.requestPermissions(
+            this,
+            permissions.toTypedArray(),
+            permissionId
+        )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun requestAccessBGLocation() {
+        Log.i("requestAccessBGLocation", "requestAccessBGLocation")
+        val permissions = mutableListOf(
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        )
+        ActivityCompat.requestPermissions(
+            this,
+            permissions.toTypedArray(),
+            permissionId
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == permissionId) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                startLocationService()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    if (!checkLocationAccessBG()) {
+                        requestAccessBGLocation()
+                    }
+                }
+            }
+        }
+    }
+}
+
+inline fun runOnApiAbove(api: Int, f: () -> Unit) {
+    if (Build.VERSION.SDK_INT > api) {
+        f()
+    }
+}
+
+@Suppress("DEPRECATION")
+fun <T> Context.isServiceRunning(service: Class<T>): Boolean {
+    return (getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
+        .getRunningServices(Integer.MAX_VALUE)
+        .any { it -> it.service.className == service.name }
 }
--- app/src/main/java/com/khue/speedmodule/speedtracking/BackgroundLocationTrackingService.kt
+++ app/src/main/java/com/khue/speedmodule/speedtracking/BackgroundLocationTrackingService.kt
@@ -9,24 +9,24 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import kotlinx.coroutines.flow.MutableStateFlow
 
 
-class BackgroundLocationService(private val context: Context) {
+class BackgroundLocationTrackingService(private val context: Context) {
 
     companion object {
         const val BROADCAST_NAME = "location.background.tracking"
         const val LOCATION = "location"
-        lateinit var instance: BackgroundLocationService
+        lateinit var instance: BackgroundLocationTrackingService
 
         @JvmStatic
-        fun getInstance(context: Context): BackgroundLocationService {
+        fun getInstance(context: Context): BackgroundLocationTrackingService {
             if (!Companion::instance.isInitialized) {
-                instance = BackgroundLocationService(context)
+                instance = BackgroundLocationTrackingService(context)
             }
             return instance
         }
     }
 
     private var receiver: MyReceiver = MyReceiver()
-    private var service: LocationUpdatesService? = null
+    private var service: LocationTrackingService? = null
 
     var location: MutableStateFlow<Location?> = MutableStateFlow(null)
 
@@ -36,8 +36,8 @@ class BackgroundLocationService(private val context: Context) {
     private val serviceConnection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName, service: IBinder) {
             bound = true
-            val binder = service as LocationUpdatesService.LocalBinder
-            this@BackgroundLocationService.service = binder.service
+            val binder = service as LocationTrackingService.LocalBinder
+            this@BackgroundLocationTrackingService.service = binder.service
             Log.i("BackgroundLocationService", "onServiceConnected")
             requestLocation()
         }
@@ -64,50 +64,50 @@ class BackgroundLocationService(private val context: Context) {
     ): Int {
         LocalBroadcastManager.getInstance(context).registerReceiver(
             receiver,
-            IntentFilter(LocationUpdatesService.ACTION_BROADCAST)
+            IntentFilter(LocationTrackingService.ACTION_BROADCAST)
         )
         if (!bound) {
-            val intent = Intent(context, LocationUpdatesService::class.java)
+            val intent = Intent(context, LocationTrackingService::class.java)
             intent.putExtra("distance_filter", distanceFilter)
             intent.putExtra("force_location_manager", forceLocationManager)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 context.startForegroundService(intent)
+                Log.i("BackgroundLocationService", "startForegroundService")
             } else {
                 context.bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+                Log.i("BackgroundLocationService", "bindService")
             }
-            //context.bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
-            Log.i("BackgroundLocationService", "bindService")
         }
 
         return 0
     }
 
     fun stopLocationService(): Int {
-        service?.removeLocationUpdates()
+        Log.i("BackgroundLocationTrackingService", "stopLocationService")
         LocalBroadcastManager.getInstance(context).unregisterReceiver(receiver)
 
         if (bound) {
             bound = false
             context.unbindService(serviceConnection)
+            service?.stopLocationTrackingService()
+        } else {
+            context.stopService(Intent(context, LocationTrackingService::class.java))
         }
-
         return 0
     }
 
     fun setAndroidNotification(title: String?, message: String?, icon: String?): Int {
-        if (title != null) LocationUpdatesService.NOTIFICATION_TITLE = title
-        if (message != null) LocationUpdatesService.NOTIFICATION_MESSAGE = message
-        if (icon != null) LocationUpdatesService.NOTIFICATION_ICON = icon
+        if (title != null) LocationTrackingService.NOTIFICATION_TITLE = title
+        if (message != null) LocationTrackingService.NOTIFICATION_MESSAGE = message
+        if (icon != null) LocationTrackingService.NOTIFICATION_ICON = icon
 
-        if (service != null) {
-            service?.updateNotification()
-        }
+        service?.startLocationTrackingService()
 
         return 0
     }
 
     fun setConfiguration(timeInterval: Long?): Int {
-        if (timeInterval != null) LocationUpdatesService.UPDATE_INTERVAL_IN_MILLISECONDS =
+        if (timeInterval != null) LocationTrackingService.UPDATE_INTERVAL_IN_MILLISECONDS =
             timeInterval
 
         return 0
@@ -118,14 +118,13 @@ class BackgroundLocationService(private val context: Context) {
      * If permission is denied, it requests the needed permission
      */
     private fun requestLocation() {
-        //service?.requestLocationUpdates()
-        service?.requestLocationUpdatesNotUseCallBack()
+        service?.startLocationTrackingService()
     }
 
     private inner class MyReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             val location =
-                intent.getParcelableExtra<Location>(LocationUpdatesService.EXTRA_LOCATION)
+                intent.getParcelableExtra<Location>(LocationTrackingService.EXTRA_LOCATION)
             if (location != null) {
                 val locationMap = mutableMapOf<String, Any>()
                 locationMap["latitude"] = location.latitude
@@ -144,7 +143,7 @@ class BackgroundLocationService(private val context: Context) {
 
                 //send location to activity
 //                Log.i("Location:", list[0].getAddressLine(0))
-                this@BackgroundLocationService.location.value = location
+                this@BackgroundLocationTrackingService.location.value = location
             }
         }
     }
